395dc63fa0dd7f84ff7deca9c4dd2cd9faf853f2
SebastienBinet <sebastien.binet.dev@gmail.com>
2016-10-05 16:01:13 -0400

    Some experimentations about skybox and sphere. Still images and videos.

---
+++ Experimentation_Unity_VR_001_360Video/Assets/MyPlayScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyPlayScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MovieTexture movTexture = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		movTexture.Play();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
